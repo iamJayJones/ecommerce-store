51da58239782fd4610bf1cbaceeed46d6e352adf
Demi-Leigh <demijefferies@gmail.com>
2022-02-01 11:54:29 +0200

    first commit

---
+++ assests/js/main.js
@@ -0,0 +1,294 @@
+let products = JSON.parse(localStorage.getItem("products"))
+  ? JSON.parse(localStorage.getItem("products"))
+  : [
+      {
+        title: "Jacket",
+        category: "Clothing",
+        price: 300.0,
+        img: "https://i.pinimg.com/474x/50/f6/f5/50f6f5dac0086caf7dc7cf2b9a031536--athletic-clothes-mens-nike-clothes.jpg",
+      },
+      {
+        title: "Jeans",
+        category: "Clothing",
+        price: 349.99,
+        img: "https://www.picclickimg.com/d/l400/pict/294650238398_/Last-NWT-American-Eagle-AEO-Mens-NeXt-Level.jpg",
+      },
+      {
+        title: "Nike Airmax 270",
+        category: "Shoes",
+        price: 2400.0,
+        img: "https://i.pinimg.com/originals/97/62/d4/9762d4a3674702bb1dc9401389a18a6a.jpg",
+      },
+      {
+        title: "Nike Airfirce",
+        category: "Shoes",
+        price: 1299.99,
+        img: "https://images.complex.com/complex/images/c_scale,f_auto,q_auto,w_1920/fl_lossy,pg_1/iot1dbjmzr7z6kfkoxrb/nike-air-force-1-lead-1?fimg-ssr-default",
+      },
+      {
+        title: "Ray-bans",
+        category: "Accessories",
+        price: 199.99,
+        img: "https://www.ray-ban.com/_repository/_resources/productscatalog/sun/images/Original-Wayfarer-Classic.jpg",
+      },
+      {
+        title: "Dry-fit",
+        category: "Accessories",
+        price: 150.0,
+        img: "https://cdna.lystit.com/photos/mrporter/fce8df5b/nike-black-Aerobill-Dri-fit-Cap.jpeg",
+      },
+    ];
+
+let cart = JSON.parse(localStorage.getItem("cart"))
+  ? JSON.parse(localStorage.getItem("cart"))
+  : [];
+
+// READ
+function readProducts(products) {
+  document.querySelector("#products").innerHTML = "";
+  products.forEach((product, position) => {
+    document.querySelector("#products").innerHTML += `
+      <div class="card">
+        <img src="${product.img}" class="card-img-top" alt="${product.title}">
+        <div class="card-body">
+          <h5 class="card-title">${product.title}</h5>
+          <p class="card-text">R${product.price}</p>
+          <div class="d-flex mb-3">
+            <input type="number" class="form-control" value=1 min=1 id="addToCart${position}">
+            <button type="button" class="btn btn-secondary ms-3" onclick="addToCart(${position})"><i class="fas fa-cart-plus"></i></button>
+          </div>
+          </div>
+          <div class="d-flex justify-content-end card-footer">
+            <button type="button" class="btn btn-primary w-50" data-bs-toggle="modal" data-bs-target="#editProduct${position}" >
+              Edit
+            </button>
+            <button type="button" class="btn btn-danger w-50 ms-3" onclick="deleteProduct(${position})" >
+              Delete
+            </button>
+          </div>
+      </div>
+              <div
+                class="modal fade"
+                id="editProduct${position}"
+                tabindex="-1"
+                aria-labelledby="exampleModalLabel"
+                aria-hidden="true"
+              >
+                <div class="modal-dialog">
+                  <div class="modal-content">
+                    <div class="modal-header">
+                      <h5 class="modal-title" id="exampleModalLabel">
+                        Edit ${product.title}
+                      </h5>
+                      <button
+                        type="button"
+                        class="btn-close"
+                        data-bs-dismiss="modal"
+                        aria-label="Close"
+                      ></button>
+                    </div>
+                    <div class="modal-body">
+                      <div class="mb-3">
+                        <label for="editTitle${position}" class="form-label">Title</label>
+                        <input
+                          class="form-control"
+                          type="text"
+                          name="editTitle${position}"
+                          id="editTitle${position}"
+                          value="${product.title}"
+                        />
+                      </div>
+                      <div class="mb-3">
+                        <label for="editCategory${position}" class="form-label">Category</label>
+                        <select
+                          class="form-select"
+                          name="editCategory${position}"
+                          id="editCategory${position}"
+                        >
+                          <option value="Fruit">Fruit</option>
+                          <option value="Vegetables">Vegetables</option>
+                          <option value="Meat">Meat</option>
+                        </select>
+                      </div>
+                      <div class="mb-3">
+                        <label for="editPrice${position}" class="form-label">Price</label>
+                        <input
+                          class="form-control"
+                          type="text"
+                          name="editPrice${position}"
+                          id="editPrice${position}"
+                          value="${product.price}"
+                        />
+                      </div>
+                      <div class="mb-3">
+                        <label for="editImg${position}" class="form-label">Image URL</label>
+                        <input
+                          class="form-control"
+                          type="text"
+                          name="editImg${position}"
+                          id="editImg${position}"
+                          value="${product.img}"
+                        />
+                      </div>
+                    </div>
+                    <div class="modal-footer">
+                      <button
+                        type="button"
+                        class="btn btn-secondary"
+                        data-bs-dismiss="modal"
+                      >
+                        Close
+                      </button>
+                      <button
+                        type="button"
+                        class="btn btn-primary"
+                        data-bs-dismiss="modal"
+                        onclick="updateProduct(${position})"
+                      >
+                        Save changes
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+    `;
+  });
+}
+
+readProducts(products);
+showCartBadge();
+
+// CREATE
+function createProduct() {
+  let title = document.querySelector("#addTitle").value;
+  let category = document.querySelector("#addCategory").value;
+  let price = document.querySelector("#addPrice").value;
+  let img = document.querySelector("#addImg").value;
+
+  try {
+    if (!title || !price || !img) throw new Error("Please fill in all fields");
+    products.push({
+      title,
+      category,
+      price,
+      img,
+    });
+    localStorage.setItem("products", JSON.stringify(products));
+    readProducts(products);
+  } catch (err) {
+    alert(err);
+  }
+}
+
+// UPDATE
+function updateProduct(position) {
+  let title = document.querySelector(`#editTitle${position}`).value;
+  let category = document.querySelector(`#editCategory${position}`).value;
+  let price = document.querySelector(`#editPrice${position}`).value;
+  let img = document.querySelector(`#editImg${position}`).value;
+
+  try {
+    if (!title || !price || !img) throw new Error("Please fill in all fields");
+    products[position] = {
+      title,
+      category,
+      price,
+      img,
+    };
+    localStorage.setItem("products", JSON.stringify(products));
+    readProducts(products);
+  } catch (err) {
+    alert(err);
+  }
+}
+
+// DELETE
+function deleteProduct(position) {
+  let confirmation = confirm(
+    "Are you sure you want to delete the selected product?"
+  );
+
+  if (confirmation) {
+    products.splice(position, 1);
+    localStorage.setItem("products", JSON.stringify(products));
+    readProducts(products);
+  }
+}
+
+// ADD TO CART
+function addToCart(position) {
+  let qty = document.querySelector(`#addToCart${position}`).value;
+  let added = false;
+  cart.forEach((product) => {
+    if (product.title == products[position].title) {
+      alert(
+        `You have successfully added ${qty} ${products[position].title} to the cart`
+      );
+      product.qty = parseInt(product.qty) + parseInt(qty);
+      added = true;
+    }
+  });
+  if (!added) {
+    cart.push({ ...products[position], qty });
+    alert(
+      `You have successfully added ${qty} ${products[position].title} to the cart`
+    );
+  }
+
+  showCartBadge();
+
+  localStorage.setItem("cart", JSON.stringify(cart));
+}
+
+// Update Cart Badge
+function showCartBadge() {
+  document.querySelector("#badge").innerHTML = cart ? cart.length : "";
+}
+
+// SORT BY CATEGORY
+function sortCategory() {
+  let category = document.querySelector("#sortCategory").value;
+
+  if (category == "All") {
+    return readProducts(products);
+  }
+
+  let foundProducts = products.filter((product) => {
+    return product.category == category;
+  });
+
+  readProducts(foundProducts);
+  console.log(foundProducts);
+}
+
+// SORT BY NAME
+
+function sortName() {
+  let direction = document.querySelector("#sortName").value;
+
+  let sortedProducts = products.sort((a, b) => {
+    if (a.title.toLowerCase() < b.title.toLowerCase()) {
+      return -1;
+    }
+    if (a.title.toLowerCase() > b.title.toLowerCase()) {
+      return 1;
+    }
+    return 0;
+  });
+  if (direction == "descending") sortedProducts.reverse();
+  console.log(sortedProducts);
+  readProducts(products);
+}
+
+// SORT BY PRICE
+
+function sortPrice() {
+  let direction = document.querySelector("#sortPrice").value;
+
+  let sortedProducts = products.sort((a, b) => a.price - b.price);
+
+  console.log(sortedProducts);
+
+  if (direction == "descending") sortedProducts.reverse();
+  readProducts(sortedProducts);
+}
